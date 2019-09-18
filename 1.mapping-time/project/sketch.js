8ce9439abc014254df474659cf6aa7b70244ddc0
leeallennyc <lee@leeallennyc.com>
2019-09-18 15:30:43 -0400

    Revert "Updating Clock 2 Sketch"
    This reverts commit bb061a4f031beaf8cf520cd4a8c98f7e97047d5b.

--- 1.mapping-time/project/sketch.js
+++ 1.mapping-time/project/sketch.js
@@ -1,112 +1,29 @@
-// Elements adapted from p5.js tutorial: Coding Challenge #74: Clock with p5.js with Ben Shiffman
-// Source: https://www.youtube.com/watch?v=E4RyStef-gY
 
 function setup() {
   // set the width & height of the sketch
-  createCanvas(600, 600);
-  angleMode(DEGREES);
+  createCanvas(400, 130)
 
-  // print the time to the console once at the beginning of the run. 
-  print('starting time:', clock());
+  // print the time to the console once at the beginning of the run. try opening up the
+  // web inspector and poking around to see the various values the clock function gives you
+  print('starting time:', clock())
+
 }
 
 function draw() {
+  // check the clock for the current time and unpack some of its fields to generate a time-string
+  var now = clock()
 
-  background(255);
-  translate(300,300);
-  rotate(-90);
+  // set the background to 'white' – you can also specify colors use integers, hex-color strings and more.
+  // note that setting the background also clears the canvas from our previous round of drawing
+  background('white')
 
-  const now = clock();
-  let hour = now.hour;
-  let min = now.min;
-  let sec = now.sec;
-  let day = now.day;
-  let month = now.month;
+  // set up typography & drawing-color
+  textFont("Anonymous Pro") // ← check index.html to see how it was loaded from google-fonts
+  textSize(42) // make it big
+  fill(100, 50, 50)
 
-  // Set the background to change from AM to PM
-  if (now.progress.day > 0.5) {
-    background (0);
-  } else {
-    background(255);
-  }
+  // draw the time string to the canvas
+  text(now.text.date, 30, 50)
+  text(now.text.time, 30, 100)
 
-  // Future iteration for making changes to week
-
-  // if (now.progress.week > 0.9) {
-  //   ellipse(0, 250, 20, 20);
-  //   strokeWeight(2);
-  //   stroke(255,0,0);
-  // }
-
-  // Setting the position, radius and color of Month
-  strokeWeight(20);
-  stroke (45, 255 , 23);
-  noFill();
-  let monthAngle = map (month, 0, 12, 0, 360);
-  arc(0, 0, 400, 400, 0, monthAngle);
-
-
-   // Setting the position, radius and color of Days
-  strokeWeight(15);
-  stroke (60, 100, 100);
-  noFill();
-  let daysPerWeekAngle = map (day, 0, 7, 0, 360);
-  arc(0, 0, 400, 400, 0, daysPerWeekAngle);
-
- // Setting the position, radius and color of Hours
-  strokeWeight(10);
-  stroke(255, 0, 0);
-  noFill();
-  let hourAngle = map (hour % 12, 0, 12, 0, 360);
-  arc(0, 0, 300, 300, 0, hourAngle); 
-
-
- // Setting the position, radius and color of Min
-  strokeWeight(10);
-  stroke(50, 35, 68);
-  noFill();
-  let minAngle = map (min, 0, 60, 0, 360);
-  arc(0, 0, 250, 250, 0, minAngle);
-
-   // Setting the position, radius and color of Secs
-  strokeWeight(10);
-  stroke(0,0,180);
-  noFill();
-  let secondAngle = map (sec, 0, 60, 0, 360);
-  arc(0, 0, 200, 200, 0, secondAngle);
-
-
-   // set up typography & drawing-color
-   fill(255, 0, 0);
-   strokeWeight(0);
-   textFont("Montserrat"); // ← check index.html to see how it was loaded from google-fonts
-   textSize(12);
-   rotate(-270);
- 
-   // draw the time string to the canvas
-   text(now.text.date, 175, 210);
-   text(now.text.time, 175, 230);
-  }
-
-  // Hour, Min, Sec Hands to be modified in future versions
-
-  // push();
-  // rotate(secondAngle);
-  // strokeWeight(5);
-  // stroke(240);
-  // line (0,0,100,0);
-  // pop();
-
-  // push();
-  // rotate(minAngle);
-  // strokeWeight(5);
-  // stroke(240);
-  // line (0,0,75,0);
-  // pop();
-
-  // push();
-  // rotate(hourAngle);
-  // strokeWeight(5);
-  // stroke(240);
-  // line (0,0,50,0);
-  // pop();
+}
