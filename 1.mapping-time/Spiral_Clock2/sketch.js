d9ca267ec0c09c4be3c8d777998ffb10cc9932da
leeallennyc <lee@leeallennyc.com>
2019-09-18 17:46:25 -0400

    Clock Sketch 2 - Motion and Math
    Using Motion and Attempting to Map to time.

--- 1.mapping-time/Spiral_Clock2/sketch.js
+++ 1.mapping-time/Spiral_Clock2/sketch.js
@@ -1,64 +1,92 @@
-let angle = 0;
-let graphics;
-let words; 
-// let spiralImage;
-// let spiralObject;
-
-// function preload() {
-//   spiralImage = loadImage('/Users/leekuczewski/Desktop/The_New_School/Repos/dvia-2019/1.mapping-time/project/variants/large/spiral_object.jpg');
-// }
-// function preload() {
-//   spiralObject = loadModel('spiral_object.obj');
-// }
+// Elements adapted from p5.js tutorial: Coding Challenge #74: Clock with p5.js with Ben Shiffman
+// Source: https://www.youtube.com/watch?v=E4RyStef-gY
 
 function setup() {
   // set the width & height of the sketch
-  createCanvas(600, 600, WEBGL);
-  graphics = createGraphics(200, 200);
-  // graphics.background(190);
+  createCanvas(600, 600);
+  angleMode(DEGREES);
 
-  words = createGraphics(300, 300);
-  words.fill(200);
-  words.textAlign(CENTER);
-  words.textSize(20);
-  words.text('test', 100, 100);
+  // print the time to the console once at the beginning of the run. 
+  print('starting time:', clock());
 }
-  // print the time to the console once at the beginning of the run. try opening up the
-  // web inspector and poking around to see the various values the clock function gives you
-  // print('starting time:', clock());
 
-
-// Using a test of 3D primatives with WEBGL: Torus rotating on the screen.
 function draw() {
-  
-  background(200);
-  rotateX(angle);
-  stroke(255);
-  texture(graphics);
-  box(100);
-  angle += 0.04;
-  // normalMaterial();
-  
 
-  // //   // import WEBGL 3D model of spiral clock design.
-  // model(spiralObject);
-}
+  background(255);
+  translate(300,300);
+  rotate(-90);
 
-  // check the clock for the current time and unpack some of its fields to generate a time-string
-  // var now = clock();
+  const now = clock();
+  let hour = now.hour;
+  let min = now.min;
+  let sec = now.sec;
+  let day = now.day;
+  let month = now.month;
 
-  // // set the background to 'white' – you can also specify colors use integers, hex-color strings and more.
-  // // note that setting the background also clears the canvas from our previous round of drawing
-  // background('white');
+  // Set the background to change from AM to PM  
 
-  // // set up typography & drawing-color
-  // textFont("Anonymous Pro"); // ← check index.html to see how it was loaded from google-fonts
-  // textSize(42); // make it big
-  // fill(100, 50, 50);
+  if (now.progress.day > 0.5) {
+    background (0);
+  } else {
+    background(255);
+  }
 
-  // // draw the time string to the canvas
-  // text(now.text.date, 30, 50);
-  // text(now.text.time, 30, 100);
+  //experimenting with for loop and motion <----- next iteration will attempt to match it seconds
+  noStroke();
+  fill(255, 0, 0);
+  for (let i = 0; i < 1; i++) {
+    ellipse(0 + sin(frameCount) * 200,
+      1+(i*100), 15, 15);
+  }
 
+  // Setting the position, radius and color of Month
+  strokeWeight(20);
+  stroke (45, 255 , 23);
+  noFill();
+  let monthAngle = map (month, 0, 12, 0, 360);
+  arc(0, 0, 400, 400, 0, monthAngle);
+
+
+   // Setting the position, radius and color of Days
+  strokeWeight(15);
+  stroke (60, 100, 100);
+  noFill();
+  let daysPerWeekAngle = map (day, 0, 7, 0, 360);
+  arc(0, 0, 350, 350, 0, daysPerWeekAngle);
+
+ // Setting the position, radius and color of Hours
+  strokeWeight(10);
+  stroke(255, 0, 0);
+  noFill();
+  let hourAngle = map (hour % 12, 0, 12, 0, 360);
+  arc(0, 0, 300, 300, 0, hourAngle); 
+
+
+ // Setting the position, radius and color of Min
+  strokeWeight(10);
+  stroke(50, 35, 68);
+  noFill();
+  let minAngle = map (min, 0, 60, 0, 360);
+  arc(0, 0, 250, 250, 0, minAngle);
+
+   // Setting the position, radius and color of Secs
+  strokeWeight(10);
+  stroke(0,0,180);
+  noFill();
+  let secondAngle = map (sec, 0, 60, 0, 360);
+  arc(0, 0, 200, 200, 0, secondAngle);
+
+
+   // set up typography & drawing-color
+   fill(255, 0, 0);
+   strokeWeight(0);
+   textFont("Montserrat"); // ← check index.html to see how it was loaded from google-fonts
+   textSize(12);
+   rotate(-270);
+ 
+   // draw the time string to the canvas
+   text(now.text.date, 175, 210);
+   text(now.text.time, 175, 230);
+  }
   
 
